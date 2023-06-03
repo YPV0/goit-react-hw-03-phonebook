c15f2abad14b86d12d60142e3d4192a50dfa63d3
YPV0 <yuriisfw@gmail.com>
2023-06-03 15:28:17 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,15 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: JSON.parse(localStorage.getItem('contacts')),
     filter: '',
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = (name, number) => {
     if (this.state.contacts.some(contact => contact.name === name)) {
